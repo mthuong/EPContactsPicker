5a3a811d388ef5dcde74d76ab16725bc3a016156
hilla <hilla@bens-MacBook-Pro.local>
2017-11-27 17:09:00 +0200

    commented code

--- Files/EPExtensions.swift
+++ Files/EPExtensions.swift
@@ -10,26 +10,26 @@ import UIKit
 import Foundation
 
 extension String {
-    subscript(r: Range<Int>) -> String? {
-        get {
-            let stringCount = self.characters.count as Int
-            if (stringCount < r.upperBound) || (stringCount < r.lowerBound) {
-                return nil
-            }
-            let startIndex = self.characters.index(self.startIndex, offsetBy: r.lowerBound)
-            let endIndex = self.characters.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
-            return self[(startIndex ..< endIndex)]
-        }
+  subscript(r: Range<Int>) -> String? {
+    get {
+      let stringCount = self.characters.count as Int
+      if (stringCount < r.upperBound) || (stringCount < r.lowerBound) {
+        return nil
+      }
+      let startIndex = self.characters.index(self.startIndex, offsetBy: r.lowerBound)
+      let endIndex = self.characters.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
+      return self[(startIndex ..< endIndex)]
     }
-    
-    func containsAlphabets() -> Bool {
-        //Checks if all the characters inside the string are alphabets
-        let set = CharacterSet.letters
-        return self.utf16.contains( where: {
-            guard let unicode = UnicodeScalar($0) else { return false }
-            return set.contains(unicode)
-        })
-    }
+  }
+  
+  func containsAlphabets() -> Bool {
+    //Checks if all the characters inside the string are alphabets
+    let set = CharacterSet.letters
+    return self.utf16.contains( where: {
+      guard let unicode = UnicodeScalar($0) else { return false }
+      return set.contains(unicode)
+    })
+  }
 }
 
 extension UIAlertController {
@@ -44,7 +44,7 @@ extension UIAlertController {
       if item.isKind(of: UILabel.self) {
         let label = item as! UILabel
         label.font = font
-      }else {
+      } else {
         updateFont(view: item, font: font)
       }
       
@@ -60,8 +60,6 @@ extension UIAlertController {
 public extension UIView {
   public func addSubviewAutoLayoutFill(_ view: UIView) {
     self.addSubview(view)
-//    view.translatesAutoresizingMaskIntoConstraints = false
-//    view.contentMode = UIViewContentMode.center
     self.addConstraint(NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal,
                                           toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0))
     self.addConstraint(NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal,
