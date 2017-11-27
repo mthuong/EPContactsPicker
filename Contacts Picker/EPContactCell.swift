d2f1c64a254436043bc1409d5f9b42d729d3941f
hilla <hilla@bens-MacBook-Pro.local>
2017-11-27 11:44:00 +0200

    another try

---
+++ Contacts Picker/EPContactCell.swift
@@ -0,0 +1,89 @@
+//
+//  EPContactCell.swift
+//  EPContacts
+//
+//  Created by Prabaharan Elangovan on 13/10/15.
+//  Copyright © 2015 Prabaharan Elangovan. All rights reserved.
+//
+
+import UIKit
+
+class EPContactCell: UITableViewCell {
+
+    @IBOutlet weak var contactTextLabel: UILabel!
+    @IBOutlet weak var contactDetailTextLabel: UILabel!
+    @IBOutlet weak var contactImageView: UIImageView!
+    @IBOutlet weak var contactInitialLabel: UILabel!
+    @IBOutlet weak var contactContainerView: UIView!
+    
+    var contact: EPContact?
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        // Initialization code
+        selectionStyle = UITableViewCellSelectionStyle.none
+        contactContainerView.layer.masksToBounds = true
+        contactContainerView.layer.cornerRadius = contactContainerView.frame.size.width/2
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func updateContactsinUI(_ contact: EPContact, indexPath: IndexPath, subtitleType: SubtitleCellValue) {
+        self.contact = contact
+        //Update all UI in the cell here
+        self.backgroundColor = UIColor.clear
+        self.contactTextLabel?.text = contact.displayName()
+        self.contactTextLabel.textColor = UIColor.white
+        updateSubtitleBasedonType(subtitleType, contact: contact)
+        if contact.thumbnailProfileImage != nil {
+            self.contactImageView?.image = contact.thumbnailProfileImage
+            self.contactImageView.isHidden = false
+            self.contactInitialLabel.isHidden = true
+        } else {
+            self.contactInitialLabel.text = contact.contactInitials()
+            self.contactInitialLabel.textColor = UIColor.white
+            self.contactInitialLabel.backgroundColor = EPGlobalConstants.Colors.lightGrey
+            self.contactImageView.isHidden = true
+            self.contactInitialLabel.isHidden = false
+        }
+    }
+    
+    func updateSubtitleBasedonType(_ subtitleType: SubtitleCellValue , contact: EPContact) {
+        
+        switch subtitleType {
+            
+        case SubtitleCellValue.phoneNumber:
+            let phoneNumberCount = contact.phoneNumbers.count
+            
+            if phoneNumberCount == 1  {
+                self.contactDetailTextLabel.text = "\(contact.phoneNumbers[0].phoneNumber)"
+            }
+            else if phoneNumberCount > 1 {
+                self.contactDetailTextLabel.text = "\(contact.phoneNumbers[0].phoneNumber) and \(contact.phoneNumbers.count-1) more"
+            }
+            else {
+                self.contactDetailTextLabel.text = EPGlobalConstants.Strings.phoneNumberNotAvaialable
+                self.contactDetailTextLabel.textColor = EPGlobalConstants.Colors.lightGrey
+            }
+        case SubtitleCellValue.email:
+            let emailCount = contact.emails.count
+        
+            if emailCount == 1  {
+                self.contactDetailTextLabel.text = "\(contact.emails[0].email)"
+            }
+            else if emailCount > 1 {
+                self.contactDetailTextLabel.text = "\(contact.emails[0].email) and \(contact.emails.count-1) more"
+            }
+            else {
+                self.contactDetailTextLabel.text = EPGlobalConstants.Strings.emailNotAvaialable
+            }
+        case SubtitleCellValue.birthday:
+            self.contactDetailTextLabel.text = contact.birthdayString
+        case SubtitleCellValue.organization:
+            self.contactDetailTextLabel.text = contact.company
+        }
+    }
+}
---
+++ Contacts Picker/EPExtensions.swift
@@ -0,0 +1,84 @@
+//
+//  EPExtensions.swift
+//  EPContactPicker
+//
+//  Created by Prabaharan Elangovan on 14/10/15.
+//  Copyright © 2015 Prabaharan Elangovan. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension String {
+    subscript(r: Range<Int>) -> String? {
+        get {
+            let stringCount = self.characters.count as Int
+            if (stringCount < r.upperBound) || (stringCount < r.lowerBound) {
+                return nil
+            }
+            let startIndex = self.characters.index(self.startIndex, offsetBy: r.lowerBound)
+            let endIndex = self.characters.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
+            return self[(startIndex ..< endIndex)]
+        }
+    }
+    
+    func containsAlphabets() -> Bool {
+        //Checks if all the characters inside the string are alphabets
+        let set = CharacterSet.letters
+        return self.utf16.contains( where: {
+            guard let unicode = UnicodeScalar($0) else { return false }
+            return set.contains(unicode)
+        })
+    }
+}
+
+extension UIAlertController {
+  func updateFont(view:UIView, font:UIFont) {
+    for item in view.subviews {
+      if item.isKind(of: UICollectionView.self) {
+        let col = item as! UICollectionView
+        for  row in col.subviews{
+          updateFont(view: row, font: font)
+        }
+      }
+      if item.isKind(of: UILabel.self) {
+        let label = item as! UILabel
+        label.font = font
+      }else {
+        updateFont(view: item, font: font)
+      }
+      
+    }
+  }
+  open override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    let font = UIFont(name: "HelveticaNeue-Medium", size: 17.0)
+    updateFont(view: self.view, font: font! )
+  }
+}
+
+public extension UIView {
+  public func addSubviewAutoLayoutFill(_ view: UIView) {
+    self.addSubview(view)
+//    view.translatesAutoresizingMaskIntoConstraints = false
+//    view.contentMode = UIViewContentMode.center
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal,
+                                          toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal,
+                                          toItem: self, attribute: .right, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal,
+                                          toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal,
+                                          toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+  }
+  
+  public func addSubviewAutoLayout(_ view: UIView) {
+    self.addSubview(view)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.contentMode = UIViewContentMode.center
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0))
+    self.addConstraint(NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+  }
+}
